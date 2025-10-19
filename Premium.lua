9863643e4b2132d20352b27f79865f7358357152
ITKen <33260@dsr.ac.th>
2025-10-19 09:41:31 +0700

    Premium.lua

--- Premium.lua
+++ Premium.lua
@@ -1,2 +1,6 @@
 script_key = script_key
 loadstring(game:HttpGet("https://raw.githubusercontent.com/33260-maker/KenHub.lua/refs/heads/main/Beta.lua"))()
+end 
+end
+script_key = script_key
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/79b5a3062b9d7e1822904f15459bc784.lua"))()
