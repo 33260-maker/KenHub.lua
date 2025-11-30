86f12f65150491855e8886e7a068f3d6a5150f94
ITKen <33260@dsr.ac.th>
2025-11-30 22:23:55 +0700

    KuoHubUi01.lua
    Signed-off-by: ITKen <33260@dsr.ac.th>

--- KENHUBUi.lua
+++ KENHUBUi.lua
@@ -1,8 +1,8 @@
 local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
 
 local Window = redzlib:MakeWindow({
-  Title = "KEN Hub",
-  SubTitle = "by Ken9999",
+  Title = "Kuo Hub",
+  SubTitle = "by kuo9999",
   SaveFolder = "testando | redz lib v5.lua"
 })
 
